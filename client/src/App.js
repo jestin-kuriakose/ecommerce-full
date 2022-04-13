347b72ff5ee6a4d2fbbb41f2c76eeec177ee6ee1
jestin-kuriakose <jestin@teslaelectronics.ca>
2022-04-13 15:53:36 -0400

    initial commit

---
+++ client/src/App.js
@@ -0,0 +1,34 @@
+import Home from './pages/Home';
+import './App.css';
+import ProductList from './pages/ProductList';
+import Product from './pages/Product';
+import Register from './pages/Register';
+import Login from './pages/Login';
+import Cart from './pages/Cart';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Redirect,
+  Navigate
+} from "react-router-dom";
+
+
+function App() {
+  const user = false;
+  return (
+    <Router>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/products/:category' element={<ProductList/>}/>
+        <Route path='/product/:id' element={<Product/>}/>
+        <Route path='/cart' element={<Cart/>}/>
+        <Route path='/login' element={user ? <Navigate to="/" /> : <Login/>}/>
+        <Route path='/register' element={<Register/>}/>
+
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
---
+++ client/src/data.js
@@ -0,0 +1,75 @@
+export const sliderItems = [
+    {
+        id: 1,
+        img: "https://i.ibb.co/Ybfw1WM/evie-s-vz3-IQy0-LOa-A-unsplash-removebg-preview.png",
+        title: "SUMMER SALE",
+        desc: "DON'T COMPROMISE ON STYLE !",
+        bg: "f5fafd"
+    },
+    {
+        id: 2,
+        img: "https://i.ibb.co/Ybfw1WM/evie-s-vz3-IQy0-LOa-A-unsplash-removebg-preview.png",
+        title: "WINTER SALE",
+        desc: "DON'T COMPROMISE ON STYLE !",
+        bg: "fcf1ed"
+    },
+    {
+        id: 3,
+        img: "https://i.ibb.co/Ybfw1WM/evie-s-vz3-IQy0-LOa-A-unsplash-removebg-preview.png",
+        title: "POPULAR SALE",
+        desc: "DON'T COMPROMISE ON STYLE !",
+        bg: "fbf0f4"
+    }
+]
+
+export const categories = [
+    {
+        id: 1,
+        img: "https://i.ibb.co/Ybfw1WM/evie-s-vz3-IQy0-LOa-A-unsplash-removebg-preview.png",
+        title: "SHIRT STYLE",
+        cat: "women"
+    },
+    {
+        id: 2,
+        img: "https://i.ibb.co/Ybfw1WM/evie-s-vz3-IQy0-LOa-A-unsplash-removebg-preview.png",
+        title: "LOUNGEWEAR LOVE",
+        cat: "coat"
+    },
+    {
+        id: 3,
+        img: "https://i.ibb.co/Ybfw1WM/evie-s-vz3-IQy0-LOa-A-unsplash-removebg-preview.png",
+        title: "LIGHT JACKETS",
+        cat: "jeans"
+    }
+]
+
+export const popularProducts = [
+    {
+        id: 1,
+        img: "https://i.ibb.co/Ybfw1WM/evie-s-vz3-IQy0-LOa-A-unsplash-removebg-preview.png",
+    },
+    {
+        id: 2,
+        img: "https://i.ibb.co/Ybfw1WM/evie-s-vz3-IQy0-LOa-A-unsplash-removebg-preview.png",
+    },
+    {
+        id: 3,
+        img: "https://i.ibb.co/Ybfw1WM/evie-s-vz3-IQy0-LOa-A-unsplash-removebg-preview.png",
+    },
+    {
+        id: 4,
+        img: "https://i.ibb.co/Ybfw1WM/evie-s-vz3-IQy0-LOa-A-unsplash-removebg-preview.png",
+    },
+    {
+        id: 5,
+        img: "https://i.ibb.co/Ybfw1WM/evie-s-vz3-IQy0-LOa-A-unsplash-removebg-preview.png",
+    },
+    {
+        id: 6,
+        img: "https://i.ibb.co/Ybfw1WM/evie-s-vz3-IQy0-LOa-A-unsplash-removebg-preview.png",
+    },
+    {
+        id: 7,
+        img: "https://i.ibb.co/Ybfw1WM/evie-s-vz3-IQy0-LOa-A-unsplash-removebg-preview.png",
+    }
+]
