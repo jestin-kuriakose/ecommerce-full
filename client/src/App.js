fcd885b95d4a474211b566c812f796f43b93578d
ilir pllana <ilirpllana95@gmail.com>
2022-04-16 03:28:20 -0400

    fixing commit issue

--- client/src/App.js
+++ client/src/App.js
@@ -24,7 +24,7 @@ function App() {
         <Route path='/product/:id' element={<Product/>}/>
         <Route path='/cart' element={<Cart/>}/>
         <Route path='/login' element={user ? <Navigate to="/" /> : <Login/>}/>
-        <Route path='/register' element={<Register/>}/>
+        <Route path='/register' element={user ? <Navigate to="/" /> : <Register/>}/>
 
       </Routes>
     </Router>
