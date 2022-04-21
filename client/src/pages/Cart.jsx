5a47cd11c001073df3880b379e7e2a3cca154b55
ilir pllana <ilirpllana95@gmail.com>
2022-04-21 16:59:16 -0400

    finished with cart page

--- client/src/pages/Cart.jsx
+++ client/src/pages/Cart.jsx
@@ -1,8 +1,12 @@
+import { Add, Remove } from "@material-ui/icons";
 import Announcement from '../components/Announcement'
 import Navbar from '../components/Navbar'
 import Footer from '../components/Footer'
 import styled from 'styled-components'
 import { useSelector } from 'react-redux'
+import StripeCheckout from "react-stripe-checkout";
+
+const KEY = process.env.REACT_APP_STRIPE
 
 const Container = styled.div``
 const Wrapper = styled.div`
@@ -43,16 +47,21 @@ const Info = styled.div`
     flex: 3;
 `
 const Product = styled.div`
-    
+    display: flex;
+    justify-content: space-between;
 `
 const ProductDetail = styled.div`
-    
+    flex: 2;
+    display: flex;
 `
 const Image = styled.img`
-    
+    width: 200px;
 `
 const Details = styled.div`
-    
+    padding: 20px;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-around;
 `
 const ProductName = styled.span`
     
@@ -61,17 +70,71 @@ const ProductId = styled.span`
     
 `
 const ProductColor = styled.div`
-    
+    width: 20px;
+    height: 20px;
+    border-radius: 50%;
+    background-color: ${(props) => props.color};
 `
 const ProductSize = styled.span`
     
 `
 const PriceDetail = styled.span`
-    
+    flex: 1;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
 `
+const ProductAmountContainer = styled.span`
+    display: flex;
+    align-items: center;
+    margin-bottom: 20px;
+`
+
+const ProductAmount = styled.div`
+  font-size: 24px;
+  margin: 5px;
+  
+`;
+
+const ProductPrice = styled.div`
+  font-size: 30px;
+  font-weight: 200;
+  
+`;
+
+const Hr = styled.hr`
+  background-color: #eee;
+  border: none;
+  height: 1px;
+`;
+
 const Summary = styled.div`
     flex: 1;
 `
+const SummaryTitle = styled.h1`
+  font-weight: 200;
+`;
+
+const SummaryItem = styled.div`
+  margin: 30px 0px;
+  display: flex;
+  justify-content: space-between;
+  font-weight: ${(props) => props.type === "total" && "500"};
+  font-size: ${(props) => props.type === "total" && "24px"};
+`;
+
+const SummaryItemText = styled.span``;
+
+const SummaryItemPrice = styled.span``;
+
+const Button = styled.button`
+  width: 100%;
+  padding: 10px;
+  background-color: black;
+  color: white;
+  font-weight: 600;
+`;
 
 const Cart = () => {
     const cart = useSelector(state=>state.cart)
@@ -93,19 +156,45 @@ const Cart = () => {
                 <Info>
                     {cart.products.map((product) => (<Product>
                         <ProductDetail>
-                            <Image src={product.img} width="120px"/>
+                            <Image src={product.img}/>
                             <Details>
                                 <ProductName><b>Product:</b>{product.title}</ProductName>
                                 <ProductId><b>Product ID:</b>{product._id}</ProductId>
-                                <ProductColor/>
+                                <ProductColor color={product.color}/>
                                 <ProductSize><b>Size:</b>{product.size}</ProductSize>
 
                             </Details>
                         </ProductDetail>
-                        <PriceDetail>Price: {product.price}</PriceDetail>
+                        <PriceDetail>
+                            <ProductAmountContainer>
+                                <Add />
+                                <ProductAmount>{product.quantity}</ProductAmount>
+                                <Remove />
+                            </ProductAmountContainer>
+                            <ProductPrice>$ {product.price*product.quantity}</ProductPrice>
+                        </PriceDetail><Hr />
                     </Product>))}
                 </Info>
-                <Summary>Summary</Summary>
+                <Summary>
+            <SummaryTitle>ORDER SUMMARY</SummaryTitle>
+            <SummaryItem>
+              <SummaryItemText>Subtotal</SummaryItemText>
+              <SummaryItemPrice>$ {cart.total}</SummaryItemPrice>
+            </SummaryItem>
+            <SummaryItem>
+              <SummaryItemText>Estimated Shipping</SummaryItemText>
+              <SummaryItemPrice>$ 5.90</SummaryItemPrice>
+            </SummaryItem>
+            <SummaryItem>
+              <SummaryItemText>Shipping Discount</SummaryItemText>
+              <SummaryItemPrice>$ -5.90</SummaryItemPrice>
+            </SummaryItem>
+            <SummaryItem type="total">
+              <SummaryItemText>Total</SummaryItemText>
+              <SummaryItemPrice>$ 80</SummaryItemPrice>
+            </SummaryItem>
+            <Button>CHECKOUT NOW</Button>
+          </Summary>
             </Bottom>
         </Wrapper>
         <Footer/>
